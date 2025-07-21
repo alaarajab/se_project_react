9ddea4a82f963078b1282a3adceb6353975dea69
Alaa Rajab <alaa88rajab@gmail.com>
2025-07-21 17:44:06 -0500

    Update Header component styling and normalize.css

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,4 +1,18 @@
+import "./Header.css";
+import logo from "../../assets/";
+import avatar from "../../assets/avatar.png";
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="header">
+      <img className="header__image" />
+      <p className="header__date-and-location">Date and Location</p>
+      <button className="header__add-clothes-btn">+ add clothes</button>
+      <div className="header__user-container">
+        <p className="header__username">Name</p>
+        <img src={avatar} alt="" className="header__avatar" />
+      </div>
+    </header>
+  );
 }
 export default Header;
