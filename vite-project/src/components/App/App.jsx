45c154ef09cd6fc985158b4bde5341afba4cb9b6
Alaa Rajab <alaa88rajab@gmail.com>
2025-07-29 14:53:26 -0500

    changes

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -4,17 +4,82 @@ import "./App.css";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import ItemModal from "../ItemModal/ItemModal";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
+  const [activeModal, setactiveModal] = useState("");
+  const [selectedCard, setSelectedCard] = useState({});
+  const handleCardClick = (card) => {
+    setactiveModal("preview");
+    setSelectedCard(card);
+  };
+  const handleAddClick = () => {
+    setactiveModal("add-garment");
+  };
+  const closeActiveModal = () => {
+    setactiveModal("");
+  };
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
-        <Main weatherData={weatherData} />
+        <Header handleAddClick={handleAddClick} />
+        <Main weatherData={weatherData} handleCardClick={handleCardClick} />
         <Footer />
       </div>
-      <ModalWithForm />
+      <ModalWithForm
+        title="New garmet"
+        buttonText="Add garmet"
+        activeModal={activeModal}
+        onClose={closeActiveModal}
+      >
+        <label htmlFor="name" className="modal__label">
+          Name{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="name"
+            placeholder="Name"
+          />
+        </label>
+        <label htmlFor="imageUrl" className="modal__label">
+          Image{" "}
+          <input
+            type="text"
+            className="modal__input"
+            id="imageUrl"
+            placeholder="Image URL"
+          />
+        </label>
+        <fieldset className="modal__radio-buttons">
+          <legend className="modal__legend">Select the weather type:</legend>
+          <label htmlFor="hot" className="modal__label modal__label_type_radio">
+            <input id="cold" type="radio" className="modal__radio-input" />
+            Hot
+          </label>
+
+          <label
+            htmlFor="warm"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="cold" type="radio" className="modal__radio-input" />
+            Warm
+          </label>
+
+          <label
+            htmlFor="cold"
+            className="modal__label modal__label_type_radio"
+          >
+            <input id="cold" type="radio" className="modal__radio-input" />
+            cold
+          </label>
+        </fieldset>
+      </ModalWithForm>
+      <ItemModal
+        activeModal={activeModal}
+        card={selectedCard}
+        onClose={closeActiveModal}
+      />
     </div>
   );
 }
